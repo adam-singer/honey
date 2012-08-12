7f8fe93ca58666726b0d5c86bd9ef58b889eaf12
rmsmith <ross@honey>
2012-08-12 13:32:57 -0700

    first draft data

---
+++ data/data.dart
@@ -0,0 +1,182 @@
+
+#library('honey:data');
+
+#import('dart:json');
+#import('../happening/happening.dart');
+
+class Data implements Hashable, Map<String, Dynamic> {
+  
+  Map<String, Dynamic> _properties;
+  
+  final Happening<String> adding;
+  final Happening<String> added;  
+  final Happening<String> changing;    
+  final Happening<String> changed;    
+  final Happening<String> removing;
+  final Happening<Dynamic> removed;
+  final Happening resetting;
+  final Happening reset;
+  
+  Data() : this._fromMap(new Map<String, Dynamic>());
+  
+  Data._fromMap(this._properties)
+  : adding = new Happening<String>()
+  , added = new Happening<String>()
+  , changing = new Happening<String>()
+  , changed = new Happening<String>() 
+  , removing = new Happening<String>()
+  , removed = new Happening<Dynamic>()
+  , resetting = new Happening()
+  , reset = new Happening() {
+    _parseMap(_properties);
+  }
+      
+  // TODO: per spec this will have builtin checks that other is not null/this
+  bool operator ==(Object other) {
+    if(this === other) return true;
+    if(null === other || !(other is Data)) return false;    
+    final Data otherData = other;
+    if(length != otherData.length) return false;
+    if(length == 0) return false; // two empty and different Data objects are not equal  
+    // TODO: I don't like that this allocates a new Collection
+    // TODO: we should probably use forEach(f) even though we can't break the loop... :(
+    // as suggested by Christopher Wright, we could throw an Exception to break !?!
+    Collection keys = _properties.getKeys();
+    for(String k in keys) {
+      if(!otherData.containsKey(k)) return false;
+      if(!_buildEquals(_properties[k], otherData[k])) return false;     
+    }    
+    return true;
+  }
+  
+  bool _buildEquals(Dynamic value, Dynamic otherValue) {
+    if(value is List) {
+      if(!(otherValue is List)) return false;
+      var list = value;        
+      var otherList = otherValue;
+      if(list.length != otherList.length) return false;        
+      for(int i = 0; i < list.length; i++) {
+        if(!_buildEquals(list[i], otherList[i])) return false;
+      }
+      return true;
+    } else {
+      return value == otherValue;
+    }
+  }
+  
+  int hashCode() {
+    // TODO: investigate using a Jenkins hash instead as described in this forum thread:
+    // https://groups.google.com/a/dartlang.org/group/misc/browse_thread/thread/bcefaa5674a73819/7f19db1217011a51#7f19db1217011a51
+    int result = 17;
+    _properties.forEach((k, v) {
+      result = 37 * result + _buildHash(v, result);
+    });
+    return result;
+  }
+  
+  int _buildHash(Dynamic value, int result) {
+    if(value != null) {
+      if(value is List) {
+        var list = value;
+        list.forEach((e) {
+          result = 37 * result + _buildHash(e, result);
+        });
+      } 
+      else if(value is bool) {
+        result = 37 * result + (value ? 0 : 1);
+      }      
+      else { // num, String, Data are all Hashable
+        result = 37 * result + value.hashCode();
+      }
+    }
+    return result;
+  }
+  
+  int get length() => _properties.length;
+  operator [](key) => _properties[key];
+  bool containsKey(key) => _properties.containsKey(key);
+  bool containsValue(value) => _properties.containsValue(value);  
+  forEach(void f(key,value)) => _properties.forEach(f);
+  Collection getKeys() => _properties.getKeys();
+  Collection getValues() => _properties.getValues();  
+  bool isEmpty() => _properties.isEmpty();
+  
+  operator []=(key,value) {
+    if(containsKey(key)) {
+      changing.happen(key);
+      _properties[key] = value;
+      changed.happen(key);
+    } else {
+      adding.happen(key);
+      _properties[key] = value;
+      added.happen(key);
+    }
+  }
+  
+  void clear() { 
+    resetting.happen();
+    _properties.clear();
+    reset.happen();
+  }
+    
+  putIfAbsent(key, ifAbsent()) {
+    final bool isAbsent = !containsKey(key);
+    if(isAbsent) adding.happen(key);
+    final value = _properties.putIfAbsent(key, ifAbsent);
+    if(isAbsent) added.happen(key);
+    return value;
+  }
+  
+  remove(key) {
+    removing.happen(key);
+    final value = _properties.remove(key);
+    removed.happen(value);
+    return value;
+  }
+  
+  noSuchMethod(String name, List args) {    
+    if(args.length == 0 && name.startsWith("get:")) {
+      final String propertyName = name.substring(4);
+      if(containsKey(propertyName)) {
+        return this[propertyName];
+      }
+    } else if(args.length == 1 && name.startsWith("set:")) {
+      final String propertyName = name.substring(4);      
+      this[propertyName] = args[0];
+      return;
+    }
+    super.noSuchMethod(name, args);
+  }
+  
+  void fromJson(String s) {    
+    resetting.happen();
+    _properties = JSON.parse(s);
+    assert(_properties is Map);
+    _parseMap(_properties);
+    reset.happen();
+  }
+  
+  String toJson() => JSON.stringify(_properties);
+  
+  void _parseList(List list) {
+    for(int i = 0; i < list.length; i++) {
+      var item = list[i];
+      if(item is Map) {
+        list[i] = new Data._fromMap(item);        
+      } else if(item is List) {
+        _parseList(item);
+      }
+    }
+  }
+  
+  void _parseMap(Map props) {    
+    props.forEach((key, value) {
+      if(value is Map) {        
+        props[key] = new Data._fromMap(value);
+      } else if(value is List) {
+        _parseList(value);
+      }
+    });
+  }
+  
+}
---
+++ data/data_test.dart
@@ -0,0 +1,323 @@
+
+#library('honey:data:test');
+
+#import('file:///c:/dart/dart-sdk/lib/unittest/unittest.dart');
+
+#import('data.dart');
+
+void main() {
+  testData();
+}
+
+class Foo extends Data {    
+  // TODO: this boilerplate is not necessary other than to appease the static checker :S
+  int get x() => this['x'];
+  set x(value) { this['x'] = value; }
+  String get y() => this['y'];
+  set y(value) { this['y'] = value; }
+  List<Bar> get l() => this['l'];
+  set l(value) { this['l'] = value; }
+  // TODO: throws in checked mode if I use Bar, not sure how to resolve..
+  // HMMM well the resolution might be if Data implements HasTypeId, and then we serialize the typeId
+  // to the JSON.  On a deserialize, we use a DataFactory to construct the Data.
+  // that sounds too complicated, better to embrace the Dynamic nature of this I think..
+  Dynamic/*Bar*/ get b() => this['b']; 
+  set b(value) { this['b'] = value; }
+  
+  Foo()
+  : super() {
+    l = new List<Bar>();
+    b = new Bar(0);
+  }
+}
+
+class Bar extends Data {  
+  int get n() => this['n'];
+  set n(value) { this['n'] = value; }
+  
+  Bar(int n)
+  : super() {
+    this.n = n;
+  }
+}
+
+var testJson = 
+"""
+{
+  "y" : "you buy posters",
+  "x" : 42,
+  "l" : [{"n":12}, {"n":17}, {"n":1000}],
+  "b" : {"n":7}
+}
+""";
+
+void testData() {  
+  group('testData', () {    
+    // serialization tests
+    test('testToJson', testToJson);    
+    test('testFromJson', testFromJson);    
+    
+    // equality / hashCode tests    
+    test('testReflexiveBarEquals', testReflexiveBarEquals);
+    test('testSymmetricBarEquals', testSymmetricBarEquals);
+    test('testTransitiveBarEquals', testTransitiveBarEquals);
+    test('testBarNotEqualsNull', testBarNotEqualsNull);
+    test('testReflexiveFooEquals', testReflexiveFooEquals);
+    test('testSymmetricFooEquals', testSymmetricFooEquals);
+    test('testTransitiveFooEquals', testTransitiveFooEquals);
+    test('testFooNotEqualsNull', testFooNotEqualsNull);    
+    test('testEmptyDataNotEquals', testEmptyDataNotEquals);
+    
+    // Map test cases
+    test('testMapAdd', testMapAdd);
+    test('testMapChange', testMapChange);
+    test('testMapPutIfAbsent', testMapPutIfAbsent);
+    test('testMapRemove', testMapRemove);
+    test('testMapClear', testMapClear);    
+  });
+}
+
+void testToJson() {
+  Foo f = new Foo();
+  f.x = 3;
+  f.y = 'snarf';  
+  f.l.add(new Bar(2));
+  f.l.add(new Bar(16));  
+  f.b = new Bar(99);  
+  expect(f.toJson(), equals('{"l":[{"n":2},{"n":16}],"y":"snarf","b":{"n":99},"x":3}'));
+}
+
+void testFromJson() {    
+  Foo f = new Foo();  
+  bool observedResetting = false;
+  bool observedReset = false;  
+  f.resetting.observe((p) {
+    observedResetting = true;
+    expect(p, isNull);
+  });  
+  f.reset.observe((p) {
+    observedReset = true;
+    expect(p, isNull);
+  });        
+  f.fromJson(testJson);  
+  expect(f.y, equals("you buy posters"));
+  expect(f.x, equals(42));
+  expect(f.l[0].n, equals(12));
+  expect(f.l[1].n, equals(17));
+  expect(f.l[2].n, equals(1000));
+  expect(f.b.n, equals(7));
+  expect(observedResetting, isTrue);
+  expect(observedReset, isTrue);
+}
+
+void testReflexiveBarEquals() {
+  Bar x = new Bar(4);
+  expect(x, equals(x));
+}
+
+void testSymmetricBarEquals() {  
+  Bar x = new Bar(4);
+  Bar y = new Bar(4);  
+  Bar z = new Bar(5);
+  expect(x, equals(y));
+  expect(y, equals(x));
+  Expect.notEquals(z, x);
+  Expect.notEquals(x, z);
+  expect(x.hashCode(), equals(y.hashCode()));
+}
+
+void testTransitiveBarEquals() {
+  Bar x = new Bar(4);
+  Bar y = new Bar(4);  
+  Bar z = new Bar(4);
+  expect(x, equals(y));
+  expect(y, equals(z));
+  expect(x, equals(z));
+  expect(x.hashCode(), equals(y.hashCode()));
+  expect(y.hashCode(), equals(z.hashCode()));
+  expect(x.hashCode(), equals(z.hashCode()));
+}
+
+void testBarNotEqualsNull() {
+  Bar x = new Bar(4);
+  Expect.notEquals(null, x);  
+}
+
+void testReflexiveFooEquals() {
+  Foo x = new Foo();
+  x.x = 3;
+  x.y = 'snarf';  
+  x.l.add(new Bar(2));
+  x.l.add(new Bar(16));  
+  x.b = new Bar(99);
+  expect(x, equals(x));
+}
+
+void testSymmetricFooEquals() {
+  Foo x = new Foo();
+  x.x = 3;
+  x.y = 'snarf';  
+  x.l.add(new Bar(2));
+  x.l.add(new Bar(16));  
+  x.b = new Bar(99);
+  Foo y = new Foo();
+  y.x = 3;
+  y.y = 'snarf';  
+  y.l.add(new Bar(2));
+  y.l.add(new Bar(16));  
+  y.b = new Bar(99);
+  Foo z = new Foo();
+  z.x = 7;
+  z.y = 'buna';  
+  z.l.add(new Bar(11));
+  z.l.add(new Bar(1));  
+  z.b = new Bar(1024);
+  expect(x, equals(y));
+  expect(y, equals(x));
+  Expect.notEquals(z, x);
+  Expect.notEquals(x, z);
+  expect(x.hashCode(), equals(y.hashCode()));
+}
+
+void testTransitiveFooEquals() {
+  Foo x = new Foo();
+  x.x = 3;
+  x.y = 'snarf';  
+  x.l.add(new Bar(2));
+  x.l.add(new Bar(16));  
+  x.b = new Bar(99);
+  Foo y = new Foo();
+  y.x = 3;
+  y.y = 'snarf';  
+  y.l.add(new Bar(2));
+  y.l.add(new Bar(16));  
+  y.b = new Bar(99);
+  Foo z = new Foo();
+  z.x = 3;
+  z.y = 'snarf';  
+  z.l.add(new Bar(2));
+  z.l.add(new Bar(16));  
+  z.b = new Bar(99);
+  expect(x, equals(y));
+  expect(y, equals(z));
+  expect(x, equals(z));
+  expect(x.hashCode(), equals(y.hashCode()));
+  expect(y.hashCode(), equals(z.hashCode()));
+  expect(x.hashCode(), equals(z.hashCode()));
+}
+
+void testFooNotEqualsNull() {
+  Foo x = new Foo();
+  Expect.notEquals(null, x);
+}
+
+void testEmptyDataNotEquals() {
+  Data x = new Data();
+  Data y = new Data();
+  Expect.notEquals(x, y);
+}
+
+void testMapAdd() {
+  Data data = new Data();  
+  int addingCount = 0;
+  int addedCount = 0;
+  data.adding.observe((value) {
+    addingCount++;
+    if(addingCount == 1) expect(value, equals("a"));
+    if(addingCount == 2) expect(value, equals("b"));
+  });  
+  data.added.observe((value) {
+    addedCount++;
+    if(addedCount == 1) expect(value, equals("a"));
+    if(addedCount == 2) expect(value, equals("b"));
+  });  
+  data['a'] = 12;
+  data['b'] = 'snarf';      
+  expect(data['a'], equals(12));
+  expect(data['b'], equals('snarf'));  
+  expect(addingCount, equals(2));
+  expect(addedCount, equals(2));  
+}
+
+void testMapChange() {
+  Data data = new Data();  
+  int addingCount = 0;
+  int addedCount = 0;
+  int changingCount = 0;
+  int changedCount = 0;  
+  data.adding.observe((value) { addingCount++; });  
+  data.added.observe((value) { addedCount++; });  
+  data.changing.observe((value) {
+    changingCount++;
+    if(changingCount == 1) expect(value, equals("a"));
+    if(changingCount == 2) expect(value, equals("b"));
+  });  
+  data.changed.observe((value) {
+    changedCount++;
+    if(changedCount == 1) expect(value, equals("a"));
+    if(changedCount == 2) expect(value, equals("b"));
+  });  
+  data['a'] = 12;
+  data['b'] = 'snarf';  
+  expect(addingCount, equals(2));
+  expect(addedCount, equals(2));
+  expect(changingCount, equals(0));
+  expect(changedCount, equals(0));  
+  data['a'] = 4096;
+  data['b'] = 'woot';  
+  expect(data['a'], equals(4096));
+  expect(data['b'], equals('woot'));  
+  expect(addingCount, equals(2));
+  expect(addedCount, equals(2));
+  expect(changingCount, equals(2));
+  expect(changedCount, equals(2));
+}
+
+void testMapPutIfAbsent() {
+  Data data = new Data();  
+  int addingCount = 0;
+  int addedCount = 0;
+  int putCount = 0;  
+  data.adding.observe((value) {
+    addingCount++;
+    if(addingCount == 1) expect(value, equals("a"));
+    if(addingCount == 2) expect(value, equals("b"));
+  });  
+  data.added.observe((value) {
+    addedCount++;
+    if(addedCount == 1) expect(value, equals("a"));
+    if(addedCount == 2) expect(value, equals("b"));
+  });    
+  data.putIfAbsent('a', () { 
+    putCount++; 
+    return 12;
+  });  
+  data.putIfAbsent('b', () { 
+    putCount++; 
+    return 'snarf';
+  });  
+  expect(addingCount, equals(2));
+  expect(addedCount, equals(2));
+  expect(putCount, equals(2));
+  data.putIfAbsent('a', () { 
+    putCount++; 
+    return 4096;
+  });  
+  data.putIfAbsent('b', () { 
+    putCount++; 
+    return 'woot';
+  });  
+  expect(data['a'], equals(12));
+  expect(data['b'], equals('snarf'));
+  expect(addingCount, equals(2));
+  expect(addedCount, equals(2));
+  expect(putCount, equals(2));
+}
+
+void testMapRemove() {
+  // TODO:
+}
+
+void testMapClear() {
+  // TODO:
+}
